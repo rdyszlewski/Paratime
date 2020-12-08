6656727c183f5f761bce9709297ae86d27c04de1
rdyszlewski <parabitsdev@gmail.com>
2020-12-08 17:38:43 +0100

    Labels style

--- src/app/shared/side-container/labels/labels.component.ts
+++ src/app/shared/side-container/labels/labels.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, ViewChild} from '@angular/core';
 import { LabelsModel } from './common/list.model';
 import { DataService } from 'app/data.service';
 import { MatDialog } from '@angular/material/dialog';
 import { LabelEditingController } from './editing/label.editing.controller';
-import { LabelAddingController } from './adding/label.adding.controller';
 import { LabelViewState } from './common/label_view_state';
 import { LabelRemovingController } from './removing/label.removing.controller';
 import {
@@ -13,6 +12,9 @@ import {
 } from '@angular/cdk/drag-drop';
 import { CommandService } from 'app/commands/manager/command.service';
 import { ChangeLabelsOrderCommand } from 'app/commands/data-command/label/command.change-labels-order';
+import { InsertingTemplateComponent } from 'app/tasks/shared/inserting-template/inserting-template.component';
+import { Label } from 'app/database/shared/label/label';
+import { CreateLabelCommand } from 'app/commands/data-command/label/command.create-label';
 
 @Component({
   selector: 'app-labels',
@@ -21,28 +23,25 @@ import { ChangeLabelsOrderCommand } from 'app/commands/data-command/label/comman
 })
 export class LabelsComponent implements OnInit {
 
+  @ViewChild(InsertingTemplateComponent)
+  private insertingTemplateComponent: InsertingTemplateComponent;
+
   public model: LabelsModel = new LabelsModel();
   public state: LabelViewState = new LabelViewState();
 
   private editingManager: LabelEditingController;
-  private addingManager: LabelAddingController;
   private removingManager: LabelRemovingController;
 
   constructor(public dialog: MatDialog, private dataService: DataService, private commandService: CommandService) {}
 
   ngOnInit(): void {
     this.editingManager = new LabelEditingController(this.state, this.commandService);
-    this.addingManager = new LabelAddingController(this.state, this.model, this.commandService);
     this.removingManager = new LabelRemovingController(this.model, this.dialog, this.commandService);
     this.loadLabels();
   }
 
   public getEditing() {
     return this.editingManager;
-  }
-
-  public getAdding() {
-    return this.addingManager;
   }
 
   public getRemoving() {
@@ -78,5 +77,20 @@ export class LabelsComponent implements OnInit {
       return;
     }
     this.commandService.execute(new ChangeLabelsOrderCommand(currentIndex, previousIndex, this.model));
+  }
+
+  public openInserting(){
+    this.insertingTemplateComponent.open();
+  }
+
+  public isInsertingOpen():boolean{
+    if(this.insertingTemplateComponent){
+      return this.insertingTemplateComponent.visible;
+    }
+  }
+
+  public addNewLabel(name:string){
+    let label = new Label(name);
+    this.commandService.execute(new CreateLabelCommand(label, this.model));
   }
 }
