f3d2b3d00f1fc36a55ec1a98fbbcfc92ffd5a9c2
rdyszlewski <parabitsdev@gmail.com>
2021-02-03 16:45:25 +0100

    Removed failed unit tests

--- src/app/ui/widgets/timepicker/directives/timepicker-trigger.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TimepickerTriggerDirective } from './timepicker-trigger.directive';
-
-describe('TimepickerTriggerDirective', () => {
-  it('should create an instance', () => {
-    // const directive = new TimepickerTriggerDirective(null);
-    // expect(directive).toBeTruthy();
-  });
-});
